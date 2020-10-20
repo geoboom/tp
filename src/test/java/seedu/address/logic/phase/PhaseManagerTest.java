e730959254899e794e718899db3ad9ffb66c2c56
geoboom <leegeorgie52@gmail.com>
2020-10-21 00:05:17 +0800

    Add newline at EOF to pass CI

--- src/test/java/seedu/address/logic/phase/PhaseManagerTest.java
+++ src/test/java/seedu/address/logic/phase/PhaseManagerTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class PhaseManagerTest {
+
     @Test
     public void assertInitialPhase_equalsLobby() {
         PhaseManager phaseManager = new PhaseManager();
