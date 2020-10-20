d4fdcbef917ec9b12cc47fe6147c045fcc12b9e0
geoboom <leegeorgie52@gmail.com>
2020-10-21 00:14:21 +0800

    Add PhaseInvalidException in catch block

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -215,7 +215,7 @@ public class MainWindow extends UiPart<Stage> {
             }
 
             return commandResult;
-        } catch (CommandException | ParseException e) {
+        } catch (CommandException | ParseException | PhaseInvalidException e) {
             logger.info("Invalid command: " + commandText);
             resultDisplay.setFeedbackToUser(e.getMessage());
             throw e;
