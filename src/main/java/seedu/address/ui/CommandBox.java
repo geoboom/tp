1fc7a7c917e5ccae0fb231f4488e459817c2970e
geoboom <leegeorgie52@gmail.com>
2020-10-21 00:09:11 +0800

    Update UI components to handle PhaseInvalidException

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -7,6 +7,7 @@ import javafx.scene.layout.Region;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.logic.phase.exceptions.PhaseInvalidException;
 
 /**
  * The UI component that is responsible for receiving user command inputs.
@@ -28,7 +29,8 @@ public class CommandBox extends UiPart<Region> {
         super(FXML);
         this.commandExecutor = commandExecutor;
         // calls #setStyleToDefault() whenever there is a change to the text of the command box.
-        commandTextField.textProperty().addListener((unused1, unused2, unused3) -> setStyleToDefault());
+        commandTextField.textProperty()
+            .addListener((unused1, unused2, unused3) -> setStyleToDefault());
     }
 
     /**
@@ -39,7 +41,7 @@ public class CommandBox extends UiPart<Region> {
         try {
             commandExecutor.execute(commandTextField.getText());
             commandTextField.setText("");
-        } catch (CommandException | ParseException e) {
+        } catch (CommandException | ParseException | PhaseInvalidException e) {
             setStyleToIndicateCommandFailure();
         }
     }
@@ -69,12 +71,14 @@ public class CommandBox extends UiPart<Region> {
      */
     @FunctionalInterface
     public interface CommandExecutor {
+
         /**
          * Executes the command and returns the result.
          *
          * @see seedu.address.logic.Logic#execute(String)
          */
-        CommandResult execute(String commandText) throws CommandException, ParseException;
+        CommandResult execute(String commandText)
+            throws CommandException, ParseException, PhaseInvalidException;
     }
 
 }
--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -16,6 +16,7 @@ import seedu.address.logic.Logic;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.logic.phase.exceptions.PhaseInvalidException;
 
 
 /**
@@ -199,7 +200,7 @@ public class MainWindow extends UiPart<Stage> {
      *
      * @see seedu.address.logic.Logic#execute(String)
      */
-    private CommandResult executeCommand(String commandText) throws CommandException, ParseException {
+    private CommandResult executeCommand(String commandText) throws CommandException, ParseException, PhaseInvalidException {
         try {
             CommandResult commandResult = logic.execute(commandText);
             logger.info("Result: " + commandResult.getFeedbackToUser());
